af5c7a66ffc491b918d6486518fe44e7ca2adb70
BlackJr-Git <juniorassosa@gmail.com>
2024-02-20 18:01:09 +0100

    feat : add checkout form

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -14,23 +14,23 @@ function Checkout() {
           <Link to={"./"}>Cliquez ici pour vous connecter</Link>
         </div>
 
-        <div className="flex items-center justify-center">
-          <div className="border-2 border-slate-600  w-3/5 p-6">
-            <h2>Details de facturation</h2>
+        <div className="flex items-between justify-center">
+          <div className="w-3/5 p-6">
+            <h2 className="text-xl font-bold py-6">Details de facturation</h2>
             <form>
                 <div className="flex justify-between gap-3">
                     <div className="flex flex-col w-1/2">
                         <label htmlFor="name">Nom *</label>
-                        <input type="text" name="name"/>
+                        <input className="p-1" type="text" name="name"/>
                     </div>
                     <div className="flex flex-col w-1/2">
                         <label htmlFor="first-name">Prenom *</label>
-                        <input type="text" name="first-name"/>
+                        <input className="p-1" type="text" name="first-name"/>
                     </div>
                 </div>
                 <div className="flex flex-col">
                     <label htmlFor="country">Pays/Region *</label>
-                    <select name="country" id="">
+                    <select name="country" id="" className="p-1">
                         <option value="Congo-Kinshasa">Congo-Kinshasa</option>
                         <option value="Congo-Brazzaville">Congo-Brazzaville</option>
                     </select>
@@ -38,48 +38,48 @@ function Checkout() {
 
                 <div className="flex flex-col">
                     <label htmlFor="adress">Adresse complète ( Numéro, Quartier, Commune )  *</label>
-                    <input type="text" name="adress" />
+                    <input className="p-1" type="text" name="adress" />
                 </div>
 
                 <div className="flex flex-col">
                     <label htmlFor="adress-2">Bâtiment, Numéro d'appartement, etc. (facultatif)</label>
-                    <input type="text" name="adress-2" />
+                    <input className="p-1" type="text" name="adress-2" />
                 </div>
 
                 <div className="flex flex-col">
                     <label htmlFor="city">Ville</label>
-                    <input type="text" name="city" />
+                    <input className="p-1" type="text" name="city" />
                 </div>
 
                 <div className="flex flex-col">
                     <label htmlFor="township">Province/Commune</label>
-                    <input type="text" name="township" />
+                    <input className="p-1" type="text" name="township" />
                 </div>
 
                 <div className="flex flex-col">
                     <label htmlFor="phone">Telephone</label>
-                    <input type="text" name="phone" />
+                    <input className="p-1" type="text" name="phone" />
                 </div>
 
                 <div className="flex flex-col">
                     <label htmlFor="phone">Telephone</label>
-                    <input type="text" name="phone" />
+                    <input className="p-1" type="text" name="phone" />
                 </div>
 
                 <div className="flex flex-col">
                     <label htmlFor="mail">Telephone</label>
-                    <input type="email" name="mail" />
+                    <input className="p-1" type="email" name="mail" />
                 </div>
 
                 <div className="flex flex-col">
                     <label htmlFor="notes">Note de commande (facultatif) </label>
-                    <textarea name="notes" id="" cols="30" rows="3"></textarea>
+                    <textarea className="p-1" name="notes" id="" cols="30" rows="3"></textarea>
                 </div>
             </form>
           </div>
 
-          <div className="border-2 border-slate-600 w-2/5">
-            <div className="max-w-6xl m-auto p-7 mb-6">
+          <div className=" w-2/5">
+            <div className="max-w-6xl m-auto p-7 mb-6 border-2 border-slate-300">
               <table className="w-full ">
                 <thead className="w-full font-bold border-b border-slate-200">
                   <td colSpan={"2"} className="py-6 text-xl">Votre commande </td>
