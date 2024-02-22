8205deb8335beb87487814f8f451f1847dddb313
BlackJr-Git <juniorassosa@gmail.com>
2024-02-22 16:13:49 +0100

    feat : show count when add products to cart

--- src/components/header/header-icons.jsx
+++ src/components/header/header-icons.jsx
@@ -1,18 +1,22 @@
 /* eslint-disable react/prop-types */
-import {Icon} from "../" 
+import { Icon } from "../";
 import { useStore } from "@/appStore";
 import { UserAvatar } from "../";
 
-
-function HeaderIcons({handleClick}) {
-    const { currentUser } = useStore();
-    return (
-        <div className="flex w-40 items-center justify-between">
-            <Icon iconName={"search-outline"} />
-            <Icon handleClick={handleClick} iconName={"cart-outline"} />
-            { currentUser.name ? <UserAvatar/>  : <Icon iconName={"person-outline"} />}
-        </div>
-    )
+function HeaderIcons({ handleClick }) {
+  const { currentUser , productsAddedToCart } = useStore();
+  return (
+    <div className="flex w-40 items-center justify-between">
+      <div className="">
+        <Icon iconName={"search-outline"} />
+      </div>
+      <div onClick={handleClick} className="relative">
+        <Icon handleClick={handleClick} iconName={"cart-outline"} />
+        <div className="flex items-center justify-center w-5 h-5 rounded-full bg-red-500 absolute -top-2 -right-3 z-0"> {productsAddedToCart.length} </div>
+      </div>
+      {currentUser.name ? <UserAvatar /> : <Icon iconName={"person-outline"} />}
+    </div>
+  );
 }
 
-export default HeaderIcons ;
+export default HeaderIcons;
