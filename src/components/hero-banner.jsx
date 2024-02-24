ac00ba41f3c821b13fbf93d6396622b875eb55ad
BlackJr-Git <juniorassosa@gmail.com>
2024-02-24 17:44:58 +0100

    style : default primary

--- src/components/hero-banner.jsx
+++ src/components/hero-banner.jsx
@@ -8,12 +8,12 @@ function HeroBanner() {
     <div className="bg-slate-100">
       <div className="hero-banner max-w-7xl m-auto rounded-2xl p-6 py-24 ">
         <div className="products-image rounded-2xl bg-white max-w-full max-h-full md:"></div>
-        <div className="hero-title flex flex-col  justify-between gap-3 bg-slate-50 p-7 rounded-2xl">
+        <div className="hero-title flex flex-col  justify-between gap-3 bg-slate-950 p-7 rounded-2xl">
           <div className="flex flex-col gap-7">
-            <h1 className="text-5xl font-bold text-slate-950">
+            <h1 className="text-5xl font-bold text-slate-50">
               Trouvez les dernières innovations tech chez Nawtech !
             </h1>
-            <p className="text-lg font-medium text-slate-950">
+            <p className="text-lg font-medium text-slate-50">
               Nous proposons les meilleurs services et ventes informatiques,
               assurés par des experts de premier plan dans le domaine.
             </p>
@@ -21,13 +21,13 @@ function HeroBanner() {
           <div className="flex flex-col gap-3">
             <Link to={"/boutique"}>
               {" "}
-              <Button className="font-bold text-xl h-12">
+              <Button className="font-bold text-xl  h-12">
                 Acheter Maintenant
               </Button>{" "}
             </Link>{" "}
             <Link to={"/boutique"}>
               {" "}
-              <Button className="w-full" variant="outline"> Voir Plus </Button>{" "}
+              <Button className="w-full text-slate-50" variant="outline"> Voir Plus </Button>{" "}
             </Link>{" "}
           </div>
         </div>
